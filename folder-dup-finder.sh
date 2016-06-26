41eed3cac6ffebb06185206576ab8273b28261a9
Riot77 <riot7seven@yahoo.com>
2016-06-26 19:28:51 +0200

    Redressed from alfa to beta

--- folder-dup-finder.sh
+++ folder-dup-finder.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
-IFS=$(echo -en "\n\b")
+#IFS=$(echo -en "\n\b")
 IFS="
 "
 #shopt -s dotglob
-RED='\033[0;31m'
+#RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 mainDir="${@}"
-allFolders="`find "${mainDir}" -type d`"
-CountAllFolders=`echo "${allFolders}" | wc -l`
+allFolders="`find "${mainDir}" -type d`"        # Get list of all folders
+CountAllFolders=`echo "${allFolders}" | wc -l`  # Count the number of all folder (for the progress bar)
 line=0
-#LogFile=folder-`date +%Y%m%d-%H%M%S`.log
-LogFile=folder.log
-echo "" > ${LogFile}
-for i in ${allFolders}; do
+LogFile=folder-`date +%Y%m%d-%H%M%S`.log        # Correct log file - contains list of folders with "same" content
+#LogFile=folder.log                             # Debug log file
+echo "" > ${LogFile}                            # Reset log file
+for origfolder in ${allFolders}; do
    # echo $i
    line=$((line+1))
    echo -n ${line}
-   for j in `echo "${allFolders}" | tail -n +${line}`; do
-      [[ "${i}" != "${j}" ]] && {
-	 #echo aaa command DIFF=diff "${i}" "${j}"
-         DIFF=`diff "${i}" "${j}"`
+   for comparefolder in `echo "${allFolders}" | tail -n +${line}`; do
+      [[ "${origfolder}" != "${comparefolder}" ]] && {
+         DIFF=`diff "${origfolder}" "${comparefolder}"`
          [[ "$?" -eq "0" ]] && {
-            echo -e "${GREEN} 100% same ${i} ${j} ${NC}"
-            echo "100% same ${i} ${j}" >> ${LogFile}
+            echo -e "${GREEN} 100% same ${origfolder} ${comparefolder} ${NC}"
+            echo "100% same ${origfolder} ${comparefolder}" >> ${LogFile}
          } || {
             LinesInDiff=`echo "${DIFF}" | wc -l` # Number of lines is equal to different files 
-            FilesInFolder1=`ls -1 "${i}" | wc -l`
-            FilesInFolder2=`ls -1 "${j}" | wc -l`
-            TotalFiles=`echo "${FilesInFolder1} + ${FilesInFolder2}" | bc -l`
+            FilesInOrigFolder=`ls -1 "${origfolder}" | wc -l`
+            FilesInCompareFolder=`ls -1 "${comparefolder}" | wc -l`
+            TotalFiles=`echo "${FilesInOrigFolder} + ${FilesInCompareFolder}" | bc -l`
             Similarity=`echo "1 - (${LinesInDiff} / ${TotalFiles})" | bc -l`
             Percentage=`echo "scale=4; ${line}/${CountAllFolders}" | bc -l`
             #echo "Total files: $TotalFiles, difffiles: $LinesInDiff, similarity: $Similarity"
-            echo -e "${Percentage}% - similar ${Similarity}% \t ${i} \t ${j}"
+            echo -e "${Percentage}% - similar ${Similarity}% \t ${origfolder} \t ${comparefolder}"
          }
       }
    done 
